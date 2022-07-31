dd198713192852f0c097c7931af1eb016670b5bc
xuanzhenzhang <xuanzhenzhang6@gmail.com>
2022-07-30 23:55:27 -0700

    abstract filter tweets function i.e. tweet gcr reply tweets

--- twitter-trader-bot/functions/config/config.js
+++ twitter-trader-bot/functions/config/config.js
@@ -1,7 +1,20 @@
 module.exports = {
-    traderIds: {data: [2837666919, 1381341090]},
-    errorObject: {
-      error: true,
-      message: 'Oops, something went wrong.Please try again later.',
-    },
-  };
+  traderIds: {
+    data: [
+      { 944686196331966464: "HsakaTrades" },
+      { 1484112393361891328: "GCRClassic" },
+      { 1289071298556170240: "GiganticRebirth" },
+      { 1154010518790791168: "HighStakesCap" },
+    ],
+  },
+  traderInfoDict: {
+    944686196331966464: "HsakaTrades",
+    1484112393361891328: "GCRClassic",
+    1289071298556170240: "GiganticRebirth",
+    1154010518790791168: "HighStakesCap",
+  },
+  errorObject: {
+    error: true,
+    message: "Oops, something went wrong.Please try again later.",
+  },
+};
